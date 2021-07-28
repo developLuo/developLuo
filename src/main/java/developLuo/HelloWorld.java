e9e9f79fe78dcdf62c9c4a2ea972ab1c5fcf401a
luoshuncun <1059549323@qq.com>
2021-07-28 22:19:03 +0800

    add helloworld file

---
+++ src/main/java/developLuo/HelloWorld.java
@@ -0,0 +1,11 @@
+package developLuo;
+
+/**
+ * author luoshuncun
+ * date 2021-07-28-22:15
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hell world");
+    }
+}
